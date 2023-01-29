a1159e6f9a2024fa13dd377e504d2a5defb3ad61
Craig Eaton <craigea92@gmail.com>
2023-01-29 17:42:56 +0000

    Added if statement to update the h1 depending on who wins the dice game

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ let randomDiceImage2 = "images/dice" + randomNumber2 + ".png" //dice1 - dice6.pn
 let image2 = document.querySelectorAll("img")[1]
 
 image2.setAttribute("src", randomDiceImage2)
+
+// If Player 1 Wins
+if (randomDiceImage > randomDiceImage2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} 
+// If Player 2 Wins
+else if (randomDiceImage < randomDiceImage2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+// If Player 1 and 2 Tie
+else {
+  document.querySelector("h1").innerHTML = "Draw!"
+}
